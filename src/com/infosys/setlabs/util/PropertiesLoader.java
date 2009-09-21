aaaecaf173d1416e1d5a349534b1bc3813bb23cd
Thomas Weibel <thom@beeblebrox.net>
2009-09-21 17:28:13 +0530

    Added new class PropertiesLoader

---
+++ src/com/infosys/setlabs/util/PropertiesLoader.java
@@ -0,0 +1,31 @@
+package com.infosys.setlabs.util;
+
+import java.util.Properties;
+
+/**
+ * Loads properties files.
+ * 
+ * @author "Thomas Weibel <thom@ikhono.net>"
+ */
+
+public abstract class PropertiesLoader {
+	/**
+	 * Loads a property file.
+	 * 
+	 * @param propertiesFile
+	 *            properties file to load
+	 * @return Loaded properties
+	 */
+	public static Properties load(String propertiesFile) {
+		Properties properties = new Properties();
+
+		try {
+			properties.load(new java.io.FileInputStream(propertiesFile));
+		} catch (Exception e) {
+			System.out.println("Error loading properties '" + propertiesFile
+					+ "'\n" + e);
+		}
+
+		return properties;
+	}
+}
