b1dc653726d17a177fab66400c66104dc5126748
Thomas Weibel <thom@beeblebrox.net>
2009-10-01 11:50:35 +0530

    Added MinerFileDAO and MinerModuleDAO interfaces

---
+++ src/com/infosys/setlabs/miner/dao/MinerFileDAO.java
@@ -0,0 +1,7 @@
+package com.infosys.setlabs.miner.dao;
+
+import com.infosys.setlabs.dao.ObjectDAO;
+import com.infosys.setlabs.miner.domain.MinerFile;
+
+public interface MinerFileDAO extends ObjectDAO<MinerFile> {
+}
---
+++ src/com/infosys/setlabs/miner/dao/MinerModuleDAO.java
@@ -0,0 +1,7 @@
+package com.infosys.setlabs.miner.dao;
+
+import com.infosys.setlabs.dao.ObjectDAO;
+import com.infosys.setlabs.miner.domain.MinerModule;
+
+public interface MinerModuleDAO extends ObjectDAO<MinerModule> {
+}
