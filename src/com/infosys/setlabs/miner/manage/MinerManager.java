417ceffde7c62bc20eb239e06e2506013b737d2c
Thomas Weibel <thom@beeblebrox.net>
2009-10-09 17:19:30 +0530

    Added empty Miner manager

---
+++ src/com/infosys/setlabs/miner/manage/MinerManager.java
@@ -0,0 +1,14 @@
+package com.infosys.setlabs.miner.manage;
+
+import java.util.HashMap;
+
+import com.infosys.setlabs.miner.common.MinerException;
+
+public class MinerManager extends Manager {
+	public MinerManager(HashMap<String, String> connectionArgs)
+			throws MinerException {
+		super(connectionArgs);
+	}
+	
+	// TODO: Move methods from Miner
+}
