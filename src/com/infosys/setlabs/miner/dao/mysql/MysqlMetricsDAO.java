2ea21cba01cace3e91870108c8a2be45b34f9863
Thomas Weibel <thom@beeblebrox.net>
2009-10-26 14:35:06 +0530

    Implemented modularization metrics

--- src/com/infosys/setlabs/miner/dao/mysql/MysqlMetricsDAO.java
+++ src/com/infosys/setlabs/miner/dao/mysql/MysqlMetricsDAO.java
@@ -1,6 +1,9 @@
 package com.infosys.setlabs.miner.dao.mysql;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import com.infosys.setlabs.dao.jdbc.JdbcDAO;
 import com.infosys.setlabs.miner.dao.FrequentItemSetDAO;
@@ -34,11 +37,11 @@ public class MysqlMetricsDAO extends JdbcDAO implements MetricsDAO {
 				+ getName();
 
 		if (codeFiles == CodeFiles.RENAMED) {
-			tmp += " WHERE has_renamed_files ";
+			tmp += " WHERE has_renamed_files";
 		}
 
 		return String.format(tmp + "))/(SELECT COUNT(id) FROM %s)) "
-				+ "AS modularization FROM %s m", tableName, tableName);
+				+ "AS modularization FROM %s", tableName, tableName);
 	}
 
 	@Override
@@ -63,7 +66,21 @@ public class MysqlMetricsDAO extends JdbcDAO implements MetricsDAO {
 
 	@Override
 	public double modularization() {
-		// TODO: modularization metrics
-		return 0;
+		double result = 0;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			ps = this.getConnection().prepareStatement(modularizationSQL());
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				result = rs.getDouble("modularization");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			this.closeResultSet(rs);
+			this.closeStatement(ps);
+		}
+		return result;
 	}
 }
