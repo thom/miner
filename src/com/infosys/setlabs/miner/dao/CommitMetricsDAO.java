29068737531ee4926a945260053adfba92abc9bf
Thomas Weibel <thom@beeblebrox.net>
2009-11-02 18:11:07 +0530

    Added commit metrics DAO

---
+++ src/com/infosys/setlabs/miner/dao/CommitMetricsDAO.java
@@ -0,0 +1,15 @@
+package com.infosys.setlabs.miner.dao;
+
+/**
+ * CommitMetrics DAO
+ * 
+ * @author Thomas Weibel <thomas_401709@infosys.com>
+ */
+public interface CommitMetricsDAO {
+	/**
+	 * Returns modularization metrics
+	 * 
+	 * @return modularization
+	 */
+	public double modularization();
+}
