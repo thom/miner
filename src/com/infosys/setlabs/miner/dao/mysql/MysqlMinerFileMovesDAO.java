71ee1e5763050501ed9e5fa3eec19d6a76f217c8
Thomas Weibel <thom@beeblebrox.net>
2010-02-23 09:37:31 +0100

    Find moves (D-A pattern within same commit ID)

--- src/com/infosys/setlabs/miner/dao/mysql/MysqlMinerFileMovesDAO.java
+++ src/com/infosys/setlabs/miner/dao/mysql/MysqlMinerFileMovesDAO.java
@@ -2,11 +2,13 @@ package com.infosys.setlabs.miner.dao.mysql;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.infosys.setlabs.dao.DataAccessException;
 import com.infosys.setlabs.dao.jdbc.JdbcDAO;
 import com.infosys.setlabs.miner.dao.MinerFileMovesDAO;
+import com.infosys.setlabs.miner.domain.MinerFile;
 
 public class MysqlMinerFileMovesDAO extends JdbcDAO implements
 		MinerFileMovesDAO {
@@ -34,13 +36,40 @@ public class MysqlMinerFileMovesDAO extends JdbcDAO implements
 		return String.format("DROP TABLE IF EXISTS %s", tableName);
 	}
 
+	protected String findMovesSQL() {
+		return String.format("SELECT f1.file_name, a1.id AS d_action_id, "
+				+ "a2.id AS a_action_id, a1.file_id AS old_file_id, "
+				+ "a2.file_id AS new_file_id, a1.commit_id "
+				+ "FROM %s a1, %s a2, files f1, files f2 "
+				+ "WHERE f1.id = a1.file_id AND f2.id = a2.file_id "
+				+ "AND f1.file_name = f2.file_name AND a1.type = 'D' "
+				+ "AND a2.type = 'A' AND a1.commit_id = a2.commit_id "
+				+ "ORDER BY a1.commit_id", tableName, tableName);
+	}
+
 	@Override
 	public void initialize() throws DataAccessException {
-		// TODO: Find moves (D-A pattern)
-		// TODO: Delete actions leading to the move (delete and add)
-		// TODO: Add new action "MINER_MOVE" with new file ID and action ID
-		// of the delete action
-		// TODO: Replace all uses of actions with miner actions
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			// Find moves (D-A pattern with same commit ID)			
+			ps = this.getConnection().prepareStatement(findMovesSQL());
+			rs = ps.executeQuery();
+			int i = 1;
+			while (rs.next()) {
+				System.out.println(i + ": " + rs.getString("file_name"));
+				i++;
+				// TODO: Modify action "A" to "MINER_MOVE"
+				// TODO: Deleted action "D"
+				// TODO: Replace all occurences of old_file_id with new_file_id in
+				// miner_actions table
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			this.closeResultSet(rs);
+			this.closeStatement(ps);
+		}
 	}
 
 	@Override
